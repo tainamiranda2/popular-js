796104fd1778f269452074ce4f8c4ea77184000e
Tainá Miranda <tainaferreira0987@gmail.com>
2023-03-14 11:23:28 -0300

    calculadora inicidada

---
+++ desafio-5/calculadora/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+ 
+  <router-view />
+</template>
+
+<style>
+
+*{
+  padding: 0;
+  box-sizing: border-box;
+  margin: 0;
+}
+.calculadora{
+  width:50%;
+
+  display: block;
+  margin:auto;
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+button {
+  background: #42b983;
+  color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+
+}
+button:hover{
+  background: #000;
+}
+.button-organizacao{
+  width: 100%;
+}
+</style>
---
+++ desafio-5/calculadora/src/views/Calculadora.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="calculadora">
+   <div class="resultado">
+    <p>{{getOperacao}}</p>
+    <p>{{getResultado}}</p>
+  </div>
+
+    <div>
+     <button @click=" dadosDel()">Del</button> 
+     <button @click=" dadosAC()">AC</button>
+     <button  @click=" dados('/')" class="operacao">/</button>
+
+    </div>
+
+    <div class="button-organizacao">
+      <button @click=" dados('7')">7</button>
+      <button  @click=" dados('8')">8</button>
+      <button  @click=" dados('9')">9</button>
+      <button @click=" dados('X')" class="operacao">x</button>
+    </div>
+    <div class="button-organizacao">
+      <button @click=" dados('6')">6</button>
+      <button @click=" dados('5')">5</button>
+      <button @click=" dados('4')">4</button>
+      <button @click=" dados('+')" class="operacao">+</button>
+    </div>
+    <div class="button-organizacao">
+      <button @click=" dados('3')">3</button>
+      <button @click=" dados('2')">2</button>
+      <button @click=" dados('1')">1</button>
+      <button @click=" dados('-')" class="operacao">-</button>
+    </div>
+    <div>
+      <button @click=" dados('0')" >0</button>
+       <button @click=" dados('.')" class="operacao">.</button>
+       <button @click="   dadosResultado()" class="operacaoMaior">=</button>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default{
+name:'Calculadora',
+data(){
+return{
+  getOperacao:"",
+  getResultado:"0",
+}
+},
+methods:{
+  dadosAC(){
+    this.getResultado="0"
+  },
+
+  dadosDel(){
+    if(this.getResultado.length>1){
+      this.getResultado=this.getResultado.substring(0, this.getResultado.length-1)
+    }else{
+      this.getResultado="0"
+    }
+  },
+
+  dados(valor){
+    if(this.getResultado=='0'){
+    this.getResultado=valor
+    }else{
+      this.getResultado+=valor
+    }
+
+  },
+  dadosResultado(){
+    let operacao=this.getResultado
+this.getOperacao=operacao
+operacao=operation.replace('X','+')
+operacao=operation.replace('/','-')
+
+this.getResultado=eval(operacao)
+  }
+}
+}
+</script>
